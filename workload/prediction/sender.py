131a00c284ec99c63970b0d1709c06b8499f0db5
진성욱 <dexter.jin@kiwiplus.io>
2021-10-10 20:37:29 +0900

    SQS Handling in Sender

--- workload/prediction/sender.py
+++ workload/prediction/sender.py
@@ -5,46 +5,69 @@ import json
 import time
 from os.path import abspath, dirname, join
 from base64 import b64encode, b64decode
-
+import random
 import requests
+import boto3, json
+
 
 import numpy as np
 
-upper_folder = abspath(dirname(dirname(__file__)))
+# Get the service resource
+sqs = boto3.client('sqs')
+
+queue_url = 'https://sqs.us-east-1.amazonaws.com/392434356039/test'
+queue_url_R = 'https://sqs.us-east-1.amazonaws.com/392434356039/alg3_R'
+queue_url_B = 'https://sqs.us-east-1.amazonaws.com/392434356039/alg3_B'
+queue_url_G = 'https://sqs.us-east-1.amazonaws.com/392434356039/alg3_G'
+queue_url_Y = 'https://sqs.us-east-1.amazonaws.com/392434356039/alg3_Y'
+queue_url_S = 'https://sqs.us-east-1.amazonaws.com/392434356039/alg3_S'
+
+global count
+count = 0
 
 
-sender = lambda data: requests.post(
-    f'http://{args.host}:{args.port}/predict/{args.name}',
-    headers={"Content-type": "application/json"},
-    data=json.dumps({ 
-        'type':'image',
-        'data': data
-    })
-)
+def sender():
+    global count
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default='127.0.0.1')
-    parser.add_argument('--port', type=int, default=7001)
-    parser.add_argument('--name', type=str, default='mx')
-    parser.add_argument('--timeout', type=int, default=490)
-    parser.add_argument('--burst', type=float, default=0.5)
-    return parser.parse_args()
+    endpoint = ["R", "B", "G", "Y", "S"]
 
-def get_data():
-    with open(f'{upper_folder}/resources/test.jpg', 'rb') as f:
-        raw_data = f.read()
-        base64_bytes = b64encode(raw_data)
-        base64_string = base64_bytes.decode('utf-8')
-        return base64_string
+    
+    try:
+        x = random.randrange(0, 100)
+        if(x >= 0 and x < 29): 
+            x = 0
+            queue_url = queue_url_R
+        if(x >= 29 and x < 38): 
+            x = 1
+            queue_url = queue_url_B
+        if(x >= 38 and x < 39): 
+            x = 2
+            queue_url = queue_url_G
+        if(x >= 39 and x < 98): 
+            x = 3
+            queue_url = queue_url_Y
+        if(x >= 98 and x < 100): 
+            x = 4
+            queue_url = queue_url_S
 
-def send_data(args, reader):
+        count += 1
+        #print(f"count {count}")
+        response = sqs.send_message(
+            QueueUrl=queue_url,
+#            MessageGroupId='messageGroup1', # for fifo
+
+            MessageBody = endpoint[x]) 
+        print("[%s]" %endpoint[x])
+        print(response)
+    except Exception as e:
+        print(e)
+
+def send_data(timeout, reader):
     pool = ThreadPoolExecutor(5000)
     data = ""
-    #get_data()
-
+    
     for row in reader:
-        if reader.line_num > args.timeout:
+        if reader.line_num > timeout:
             break
 
         num = int(int(row['tweets']) * 1.8)
@@ -59,11 +82,8 @@ def send_data(args, reader):
         print(len(samples))
         for s in samples:
             pool.submit(sender, data)
-            # sender(data)
-            # print(f'Send request after {s} ms')
             time.sleep(s/1000.0)
 
 with open(f'./tweet_load.csv', 'r') as f:
-    args = get_args()
     reader = csv.DictReader(f)
-    send_data(args, reader)
+    send_data(reader)
